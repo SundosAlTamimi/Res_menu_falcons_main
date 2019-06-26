75f605fab44817db54eca614526a79a5a53d43d2
abeer <sundos.tamomo20@gmail.com>
2019-06-26 13:59:50 +0300

    abeer support languages (loacaleAppUtils)

--- app/src/main/java/com/falconssoft/menurestaurant/LoginActivity.java
+++ app/src/main/java/com/falconssoft/menurestaurant/LoginActivity.java
@@ -6,16 +6,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.falconssoft.menurestaurant.Models.Users;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     private EditText username, password;
+    private TextView english, arabic;
     private Button login;
     private DatabaseHandler databaseHandler;
     private List<Users> users = new ArrayList<>();
@@ -32,52 +35,57 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
         username = findViewById(R.id.login_username);
         password = findViewById(R.id.login_password);
+        english = findViewById(R.id.login_language_english);
+        arabic = findViewById(R.id.login_language_arabic);
         login = findViewById(R.id.login_button);
 
         login.setOnClickListener(this);
+        english.setOnClickListener(this);
+        arabic.setOnClickListener(this);
 
     }
 
     @Override
     public void onClick(View v) {
-        users = databaseHandler.getAllUSER();
-        boolean found = false;
-        String usernameText = username.getText().toString();
-        String passwordText = password.getText().toString();
-        if (usernameText != null && passwordText != null){
-            for (int i = 0; i<users.size() ; i++)
-                if (usernameText == users.get(i).getUserName())
-                    if (passwordText.equals(users.get(i).getUserPassword())){
-                        found = true;
-                        Intent categoryIntent = new Intent(LoginActivity.this, CategoryActivity.class );
-                        startActivity(categoryIntent);
+
+        switch (v.getId()) {
+            case R.id.login_button:
+                users = databaseHandler.getAllUSER();
+                boolean found = false;
+                String usernameText = username.getText().toString();
+                String passwordText = password.getText().toString();
+                if (usernameText != null && passwordText != null) {
+                    for (int i = 0; i < users.size(); i++)
+                        if (usernameText == users.get(i).getUserName())
+                            if (passwordText.equals(users.get(i).getUserPassword())) {
+                                found = true;
+                                Intent categoryIntent = new Intent(LoginActivity.this, CategoryActivity.class);
+                                startActivity(categoryIntent);
+                            }
+
+                    if (found == false) {
+                        Toast.makeText(this, "Wrong in username or password!", Toast.LENGTH_SHORT).show();
                     }
-
-            if (found == false){
-                Toast.makeText(this, "Wrong in username or password!", Toast.LENGTH_SHORT).show();
-            }
-
-        }else {
-            username.setError("Required field!");
-            password.setError("Required field!");
-
+                } else {
+                    username.setError("Required field!");
+                    password.setError("Required field!");
+                }
+                break;
+            case R.id.login_language_english:
+                LocaleAppUtils.setLocale(new Locale("en"));
+                LocaleAppUtils.setConfigChange(this);
+                finish();
+                startActivity(getIntent());
+                break;
+            case R.id.login_language_arabic:
+                LocaleAppUtils.setLocale(new Locale("ar"));
+                LocaleAppUtils.setConfigChange(this);
+                finish();
+                startActivity(getIntent());
+                break;
         }
 
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 }
